c890414716e5df036f8f15e2ca21ca2d188da94d
Liliia Fodorova <nevskay22@gmail.com>
2026-06-19 23:40:28 +0200

    Set base path for YachtJet project

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/YachtJet/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
